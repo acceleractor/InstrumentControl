e23a4f7fb5c48e7dde17f25a75488655d240a770
4Cc3 <74807667+acceleractor@users.noreply.github.com>
2023-07-17 17:42:09 +0800

    Add files via upload
    new version

--- NI-PXle1083/Cpp源码/V.cpp
+++ NI-PXle1083/Cpp源码/V.cpp
@@ -47,8 +47,9 @@
 
 #include <stdio.h>
 #include "NIDAQmx.h"
-#include<windows.h>
 #include<iostream>
+#include<fstream>
+
 #define DAQmxErrChk(functionCall) if( DAQmxFailed(error=(functionCall)) ) goto Error; else
 
 
@@ -61,15 +62,24 @@ int main(void)
 	void*	taskHandle=0;
 	char		errBuff[2048]={'\0'};
 	double     data[1] = {1.0};
+	ofstream outlog;
+	outlog.open("outLog.txt");
+
 	/*********************************************/
 	// DAQmx Configure Coden
 	/*********************************************/
+	cout<<"set Volt"<<endl;
 	
+	outlog<<"set Volt"<<endl;
+
+	std::cin>>data[0];	
 	
 	DAQmxErrChk (DAQmxCreateTask("",&taskHandle));
 	printf("task creat success\n");
+	outlog<<"task creat success"<<endl;
 	DAQmxErrChk (DAQmxCreateAOVoltageChan(taskHandle,"PXI1Slot2/ao0","",-10.0,10.0,DAQmx_Val_Volts,""));
 	printf("AOvoltageChan creat success\n");
+	outlog<<"AOvoltageChan creat success"<<endl;
 
 	/*********************************************/
 	// DAQmx Start Code
@@ -77,15 +87,16 @@ int main(void)
 
 	DAQmxErrChk (DAQmxStartTask(taskHandle));
 	printf("task start success\n");
+	outlog<<"task start success"<<endl;
 
 	/*********************************************/
 	// DAQmx Write Code
 	/*********************************************/
 	// printf("type in Setvolt\n");
 
-	cin>>data[0];
 	DAQmxErrChk (DAQmxWriteAnalogF64(taskHandle,1,1,10.0,DAQmx_Val_GroupByChannel,data,NULL,NULL));
 	printf("analog write success\n");
+	outlog<<"analog write success"<<endl;
 
 Error:
 	if( DAQmxFailed(error) )
@@ -100,6 +111,9 @@ Error:
 	if( DAQmxFailed(error) )
 		printf("DAQmx Error: %s\n",errBuff);
 	printf("End of program, press Enter key to quit\n");
+	cout<<"you has set "<< data[0] <<"";
+	outlog<<"you has set "<< data[0] <<"";
+	
 	getchar();
 
 
